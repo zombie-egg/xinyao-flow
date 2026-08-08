b91d65436262fffb9fc0ab6941017a53f4e62ac3
Ruiqi Tang <3403615078@qq.com>
2026-08-09 02:02:53 +0800

    Handle oversized invoice uploads

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-  experimental: { serverActions: { bodySizeLimit: '8mb' } },
+  experimental: { serverActions: { bodySizeLimit: '8mb' }, middlewareClientMaxBodySize: '25mb' },
 };
 export default nextConfig;
